cc4a4b2e4ebeef308defd3a6dcbf31795fedf385
Anjali Durgam <anjalidurgam8090@gmail.com>
2026-09-05 17:33:36 +0530

    Time: 2 ms (83.18%), Space: 47.7 MB (16.48%) - LeetHub

---
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        if(arr.length < 3) {
+            return false;
+        }
+        int i = 0;
+        while(i < arr.length - 1 && arr[i] < arr[i + 1] ) {
+           i++;
+        }
+        if(i == arr.length-1 || i==0) return false;
+
+        int j = arr.length -1;
+
+         while(j > 0 && arr[j] < arr[j - 1] ) {
+           j--;
+        }
+    return i == j;
+ 
+    }
+}
